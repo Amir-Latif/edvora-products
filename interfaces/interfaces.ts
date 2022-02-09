9099a5459c8f1f71e1165ce674571f07951e2499
Amir Latif <amirwesa@gmail.com>
2022-02-09 21:30:13 +0200

    filter sidebar

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,10 @@
+export interface product {
+  product_name: string;
+  brand_name: string;
+  price: number;
+  address: { state: string; city: string };
+  discription: string;
+  date: string;
+  time: string;
+  image: string;
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// Sass Support
+const path = require("path");
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
