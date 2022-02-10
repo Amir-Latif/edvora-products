17fb439b29bf681d3df3989a4bd5f747d367a5b3
Amir Latif <amirwesa@gmail.com>
2022-02-10 12:54:00 +0200

    carousel side

---
+++ pages/components/Carousel.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { Stack } from "react-bootstrap";
+import { product } from "../../interfaces/interfaces";
+import Crs from "../../styles/Carousel.module.scss";
+import Styles from "../../styles/Index.module.scss";
+
+export default function Carousel({
+  productList,
+  brand,
+}: {
+  productList: product[];
+  brand: string;
+}) {
+  const products = productList.filter(
+    (product) => product.brand_name === brand
+  );
+
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(1);
+  const [displayedList, setDisplayedList] = useState([products[first]]);
+
+  const [desktopView, setDesktopView] = useState(false);
+
+  //   Listen to the screen size
+  useEffect(() => {
+    if (window.matchMedia("(min-width: 1366px)").matches) {
+      setDesktopView(true);
+      setDisplayedList([products[first], products[second]]);
+    }
+  });
+
+  return (
+    <React.Fragment>
+      <div className={Styles.brand}>{brand}</div>
+      <Stack direction="horizontal">
+        <div className={`${Styles.itemsContainer} ${Crs.container}`}>
+          <div className={`${Styles.itemList} d-flex`}>
+            {displayedList.map((p, i) => (
+              <div key={i} className={`${Styles.item} ${Crs.item}`}>
+                <img src={p.image} className={`${Crs.image} d-block`} />
+                <div>
+                  <div className={Crs.productName}>{p.product_name}</div>
+                  <div className={Crs.brandName}>{p.brand_name}</div>
+                  <div className="d-flex align-items-baseline">
+                    <div className={Crs.dollar}>
+                      <img src="images/dollar.svg" alt="dollar sign" />
+                    </div>{" "}
+                    <div className={Crs.price}>{p.price}</div>
+                  </div>
+                </div>
+                <div className={Crs.location}>{p.address.state}</div>
+                <div>
+                  <span className={Crs.date}>Date: </span>
+                  <span className={Crs.dateValue}>
+                    {new Date(p.date).toLocaleDateString().replaceAll("/", ":")}
+                  </span>
+                </div>
+                <div className={Crs.description}>{p.discription}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <img
+          src="images/rightArrow.svg"
+          alt="show next items"
+          className={Crs.rightArrow}
+          onClick={() => {
+            if (desktopView) {
+              first === products.length - 1 ? setFirst(0) : setFirst(first + 1);
+              second === products.length - 1
+                ? setSecond(0)
+                : setSecond(second + 1);
+              setDisplayedList([products[first], products[second]]);
+            } else {
+              first === products.length - 1 ? setFirst(0) : setFirst(first + 1);
+              setDisplayedList([products[first]]);
+            }
+          }}
+        />
+      </Stack>
+    </React.Fragment>
+  );
+}
